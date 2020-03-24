2ee0d08eb0382cfb455a106b9f91c4b469875494
Funck Dunck <justhappyduck@tutanota.com>
2020-03-24 16:21:23 +0200

    Start websocket server

---
+++ src/main/java/web/socket/server/RunSpringBootApplication.java
@@ -0,0 +1,14 @@
+package web.socket.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunSpringBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RunSpringBootApplication.class, args);
+	}
+
+}
